a8edeafdb119e4b409a838a21a4c2a34bdac357c
Eddie Pedersen <eddiepedersen90@gmail.com>
2017-12-05 04:33:41 +0000

    not even close to finished product

--- hangman.js
+++ hangman.js
@@ -29,8 +29,12 @@ inquirer.prompt({
 // I did not use my time wisely over the weekend and now I have to turn in an embarrasing piece of homework. 
 // I'll require some tutoring for the Advanced Javascript part and just scheduled one this week to go over this.
 // Based on the inclass exercises there are a few pieces missing to this homework.
-// One of the first things that needs to happen is to randomize with math.floor the word from the wordList
-// Then a function that uses 'inquire.prompt' so the user can input the letter and then with an if-else statement check to see if it matches
-// At the same time the remainingAttempts from 10 need to countdown for each letter guessed
+
+// a NewGame function is missing which:
+//      sets the number of remainingAttempts to 10
+//      uses math.floor(Math.random) to pick a word from the wordList
+//      
+// Then a 'GuessLetter' function that uses 'inquire.prompt' so the user can input the letter and then with an if-else statement check to see if it matches
+// 
 // At the end an UpdateScore is necessary 
 // If word is guessed correctly, a newGame function is required to start over
